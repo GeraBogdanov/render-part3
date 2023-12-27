c5274a96412546bee3c5f54007451e6f0e5572de
Gary Crosby <21145626+GeraBogdanov@users.noreply.github.com>
2023-12-27 22:23:13 +0300

    Changed fetching, posting, separated Mongoose code to phone.js

--- index.js
+++ index.js
@@ -1,65 +1,93 @@
+require("dotenv").config();
 const express = require("express");
-const morgan = require('morgan');
-const cors = require('cors');
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
+const Phone = require("./models/phone");
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 app.use(cors());
 app.use(express.json());
 
-morgan.token('body', req => {
-  return JSON.stringify(req.body)
-})
+morgan.token("body", (req) => {
+  return JSON.stringify(req.body);
+});
 
-app.use(morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms',
-    tokens['body'](req),
-  ].join(' ')
-}))
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens["body"](req),
+    ].join(" ");
+  })
+);
 
+// let phonebook = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
+// if (process.argv.length === 5) {
+//   const phone = new Phone({
+//     name: process.argv[3],
+//     number: process.argv[4],
+//   });
 
-let phonebook = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+//   phone.save().then((result) => {
+//     console.log(`Added ${result.name} ${result.number}`);
+//     mongoose.connection.close();
+//   });
+// }
+
+// if (process.argv.length === 3) {
+//   console.log("phonebook:");
+//   Phone.find({}).then((result) => {
+//     result.forEach((phone) => {
+//       console.log(`${phone.name} ${phone.number}`);
+//     });
+//     mongoose.connection.close();
+//   });
+// }
 
 app.get("/api/persons", (request, response) => {
-  response.json(phonebook);
+  Phone.find({}).then((result) => {
+    response.json(result);
+    // mongoose.connection.close();
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = phonebook.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Phone.findById(request.params.id).then((person) => {
+    // if (person.name) {
+      response.json(person);
+    // } else {
+    //   response.status(404).end();
+    // }
+  });
 });
 
 app.get("/info", (request, response) => {
@@ -84,29 +112,31 @@ function generateId() {
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
+
   if (!body.name) {
     return response.status(400).json({
       error: "content missing",
     });
   }
-  if(phonebook.find(person => person.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    })
-  }
 
-  const phone = {
-    name: body.name,
-    number: body.number,
-    id: generateId(),
-  };
+  Phone.find({}).then((result) => {
+    if (result.find((person) => person.name === body.name)) {
+      return response.status(400).json({
+        error: "name must be unique",
+      });
+    }
+    const phone = new Phone({
+      name: body.name,
+      number: body.number,
+    });
 
-  phonebook = phonebook.concat(phone);
-
-  response.json(phone);
+    phone.save().then((savedPhone) => {
+      response.json(savedPhone);
+    });
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
