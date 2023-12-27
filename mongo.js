8fb5b05b86ec354488924dce26a928efd7fff96b
Gary Crosby <21145626+GeraBogdanov@users.noreply.github.com>
2023-12-27 20:53:43 +0300

    connected to mongodb

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://garilasssss:${password}@cluster0.may4kvw.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const phoneSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Phone = mongoose.model("Phone", phoneSchema);
+
+if (process.argv.length === 5) {
+  const phone = new Phone({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  phone.save().then((result) => {
+    console.log(`Added ${result.name} ${result.number}`);
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length === 3) {
+  console.log("phonebook:");
+  Phone.find({}).then((result) => {
+    result.forEach((phone) => {
+      console.log(`${phone.name} ${phone.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
