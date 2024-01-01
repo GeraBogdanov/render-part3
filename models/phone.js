25d80dfba795153b5e81d7f849836dacaedb8aa4
Gary Crosby <21145626+GeraBogdanov@users.noreply.github.com>
2024-01-01 21:57:03 +0300

    uibuild

--- models/phone.js
+++ models/phone.js
@@ -22,8 +22,13 @@ const phoneSchema = new mongoose.Schema({
   },
   number:{
     type: String,
-    minLength: 3,
-    required: true
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{6,}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
   }
 });
 
